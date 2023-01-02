ae147e20c1ade87956fc950742886574af0cd4f1
karengalan <karengalan151@gmail.com>
2023-01-02 10:00:12 -0300

    subo tp terminado

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import '../App.css';
+import React from 'react';
+
+import SideBar from './SideBar';
+
+function App() {
+  return (
+    <React.Fragment>
+      	<div id="wrapper">
+          <SideBar />
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
